5856365b841797b5ae53a5a56799890e11685e39
Fredrik Stenmark <fl22bx@student.lnu.se>
2018-09-11 11:10:50 +0200

    second

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php 
 require_once('models/hangMan.php');
-		
+require_once('view/letterInput.php');		
 		$test = new hangMan();
 
 		echo $test->getWord();
- ?>
+ 
+		$view = new letterInput();
+		echo $view->show();
--- view/letterInput.php
+++ view/letterInput.php
@@ -1,7 +1,20 @@
 <?php 
 
 class letterInput {
+	private static $TEXT_FIELD_ID = "letter";
 	
+	function show () : string {
+		return "
+			<form method='get'>
+				<input type='text' letter='" . self::$TEXT_FIELD_ID .  "' ></input>
+				<input type='submit' >
+			</form>
+		" ;
+	}
+
+	function getLetter () {
+		return isset($_GET[self::$TEXT_FIELD_ID]);
+	}
 
 
 }
